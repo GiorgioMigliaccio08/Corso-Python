78999a0c515424131dae516cf9a87fc69bbb26d1
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2025-01-17 17:07:39 +0100

    Primi passi in Python

---
+++ main.py
@@ -0,0 +1,18 @@
+#Commento in Python : File creato il 17 Gennaio 2025
+#La prima cosa da fare prima di creare un progetto è crere il virtual environment per il singolo
+#proggetto , quindi avvio il terminale dentro la cartella e scrivo: python-m venv nomeprogetto.
+
+print("ciao") 
+
+#Stampo un Messaggio a schermo 
+
+messaggio = input("Inserisci qualcosa:  ") # assegno un comando di imput utente
+print (messaggio) #e lo rimando a schermo sul terminale
+
+#In Python e molto importante l'indendazione / tabulazione con gli spazi per distinguere le parti di codice
+#perche in questo linguaggio non esistono ;
+
+if 1 < 5:
+    print("Numero inserito minore di 5") #questo pezzotto di codice e più avanti rispetto alla riga superiore
+
+
