794e3483da9b3fb621e5b4aaf1d8c2e326121538
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2025-01-17 18:35:10 +0100

    Variabili

--- main.py
+++ main.py
@@ -12,7 +12,27 @@ print (messaggio) #e lo rimando a schermo sul terminale
 #In Python e molto importante l'indendazione / tabulazione con gli spazi per distinguere le parti di codice
 #perche in questo linguaggio non esistono ;
 
-if 1 < 5:
-    print("Numero inserito minore di 5") #questo pezzotto di codice e più avanti rispetto alla riga superiore
+#if 1 < 5:
+    #print("Numero inserito minore di 5") #questo pezzotto di codice e più avanti rispetto alla riga superiore
+
+
+
+#LE VARIABILI : 
+
+# - cos'e e come creare una variabile
+# Una variabile è un contenitore di un valore/oggetto che può cambiare nel tempo.
+
+# - nomenclatura : un nome comprensibile a noi
+#variabileUno #camelCase    
+#variabile_due #snake_case 
+#VARIABILETRE #altro caso 
+#nomi di variabili non consentiti: 4variabile / quinta-variabile / sesta variabile 
+
+# - assegnare multipli valori
+# - utilizzare una variabile
+# x, y, z = 12, 20, 30
+x = 10
+print(x)
+
 
 
