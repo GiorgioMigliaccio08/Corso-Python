a4e4d406f325a096dea9de5db41084b637d30852
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2025-01-20 16:46:18 +0100

    Teoria collezioni dati

---
+++ collezionidati.py
@@ -0,0 +1,12 @@
+# Le liste sono collezioni ordinate e modificabili. Permettono duplicati.
+# Le tuple sono collezioni ordinate ma immutabili. Permettono duplicati.
+
+# I set sono collezioni non ordinate e perciò non indicizzate . Non permettono duplicati.
+# I dictionary sono collezioni ordinate e modificabili (dalla versione 3.7). Non permettono duplicati.
+
+# Termini :
+# - Ordinato: la collezione ha un ordine ben definito e l'aggiunta di elementi non incide.
+# - Indicizzato: possiamo accedere agli elementi tramite indice (index)
+# - Modificabile: possiamo aggiungere , cambiare e rimuovere elementi una volta creata la collezione.
+# - Immutabile: non possiamo aggiungere , cambiare e rimuovere elementi.
+# - Permette duplicati : possono esserci più elementi con lo stesso valore.
