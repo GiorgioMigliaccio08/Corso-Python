aa55c9098b4fba217e43207caa669d014695ba3b
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2025-01-31 17:42:28 +0100

    Ereditarietà

--- classieoggetti.py
+++ classieoggetti.py
@@ -12,24 +12,44 @@ persona200 = persona()
 class Persona:
     def __init__(self, nome, cognome):
        self.nome = nome
-       self.consognome = cognome
+       self.cognome = cognome
 
     def saluta(self):
         print("Ciao sono " + self.nome)
 
-persona1 = Persona("Luca", "Rossi")
-persona2 = Persona("Valentino", "Bisegna")
+persona101 = Persona("Luca", "Rossi")
+persona201 = Persona("Valentino", "Bisegna")
 
-print(persona1.nome)
+print(persona101.nome)
 
-persona2.saluta()
+persona201.saluta()
 
 # Il Self serve a quale istanza ci stiamo riferendo
 
 # Modificare ed eliminare proprietà di un oggetto
 
-persona2.nome = "Maria"
-persona2.saluta()
+persona201.nome = "Maria"
+persona201.saluta()
 
-#del persona2.nome   per eliminare
-#del persona2       mi elimina completamente l'oggetto
+#del persona201.nome   per eliminare
+#del persona201      mi elimina completamente l'oggetto
+
+
+# EREDITARIETA'
+
+class Insegnante(Persona):
+    def __init__(self, nome, cognome, materia):
+        super().__init__(nome, cognome)
+        self.materia = materia
+
+    def saluta(self):
+        print("Buongiorno sonon l'insegnante " + self.nome + " " + self.cognome)
+
+    def dai_voto(self):
+        print("Bravo Giorgio stai imparando Python ti meriti un bel 10")
+
+insegnante1 = Insegnante("Anna", "Neri", "Matematica")
+
+print(insegnante1.materia)
+insegnante1.saluta()
+insegnante1.dai_voto()
